ed7d4c01d06b61fdae54a49a6706f963e4029739
Joris P <71002181+Miisterone@users.noreply.github.com>
2022-11-21 12:07:47 +0100

    Button Back

--- app/src/main/java/com/example/wyther/MainActivity.java
+++ app/src/main/java/com/example/wyther/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.wyther;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    final Context context = this;
+    private Button button;
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("Quittez l'application ?")
+                .setMessage("Etes vous sur de vouloir quitter l'application ?")
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(android.R.string.yes, (arg0, arg1) -> {
+                    finish();
+                    System.exit(0);
+                }).create().show();
     }
 }
