17932e5a71f2d73b9213cc68a0d0f31a2d672413
Clément Addario <71002854+ClementAdd@users.noreply.github.com>
2022-12-12 11:29:02 +0100

    menu navigation

--- app/src/main/java/com/example/wyther/MainActivity.java
+++ app/src/main/java/com/example/wyther/MainActivity.java
@@ -1,19 +1,40 @@
 package com.example.wyther;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // drawer layout instance to toggle the menu icon to open
+        // drawer and back button to close drawer
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     final Context context = this;
@@ -29,5 +50,30 @@ public class MainActivity extends AppCompatActivity {
                     finish();
                     System.exit(0);
                 }).create().show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickSettings(MenuItem item) {
+        System.out.println("Settings");
+    }
+
+    public void onClickFiltre(MenuItem item) {
+        System.out.println("filtre");
+    }
+
+    public void onClickFav(MenuItem item) {
+        System.out.println("fav");
+    }
+
+    public void onClickAbout(MenuItem item) {
+        System.out.println("about");
     }
 }
