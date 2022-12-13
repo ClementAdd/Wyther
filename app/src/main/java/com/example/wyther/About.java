6e8eb663dd81ea935ccd7fea5892b98932883f65
Clément Addario <71002854+ClementAdd@users.noreply.github.com>
2022-12-13 09:55:20 +0100

    Settings page & intents

---
+++ app/src/main/java/com/example/wyther/About.java
@@ -0,0 +1,12 @@
+package com.example.wyther;
+
+import static android.content.Intent.getIntent;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class About {
+
+    //get the intent
+
+}
---
+++ app/src/main/java/com/example/wyther/Settings.java
@@ -0,0 +1,17 @@
+package com.example.wyther;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Settings extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+
+    }
+
+}
