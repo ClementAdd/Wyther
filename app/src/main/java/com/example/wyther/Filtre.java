e5549126b39dd845a80f46a72dc8f4e2bf17f21e
Clément Addario <71002854+ClementAdd@users.noreply.github.com>
2023-01-16 12:16:56 +0100

    add menu on some pages

--- app/src/main/java/com/example/wyther/Filtre.java
+++ app/src/main/java/com/example/wyther/Filtre.java
@@ -1,17 +1,69 @@
 package com.example.wyther;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class Filtre extends AppCompatActivity {
-
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.filtre);
 
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickSettings(MenuItem item) {
+        Intent intent = new Intent(this, Settings.class);
+        //animation
+        startActivity(intent);
+    }
+
+    public void onClickFiltre(MenuItem item) {
+        Intent intent = new Intent(this, Filtre.class);
+        startActivity(intent);
+    }
+
+    public void onClickFav(MenuItem item) {
+        Intent intent = new Intent(this, Favorite.class);
+        startActivity(intent);
+
+    }
+
+    public void onClickAbout(MenuItem item) {
+        Intent intent = new Intent(this, About.class);
+        this.startActivity(intent);
+    }
+
+    public void onClickHome(MenuItem item) {
+        Intent intent = new Intent(this, MainActivity.class);
+        this.startActivity(intent);
+    }
 }
